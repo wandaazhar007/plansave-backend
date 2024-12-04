27877d35f37238ae077682340e95c067c10fb111
wanda azhar <wandaazhar@gmail.com>
2024-12-03 22:07:12 -0700

    add TransactionModel.js

---
+++ models/TransactionModel.js
@@ -0,0 +1,18 @@
+import { connectDB } from "../config/dbConfig.js";
+
+const db = await connectDB();
+
+export const getTransactionsByUserId = async (userId) => {
+  const query = `SELECT * FROM transactions WHERE user_id = ? ORDER BY date DESC`;
+  const [rows] = await db.execute(query, [userId]);
+  return rows;
+};
+
+export const createTransaction = async (userId, budgetId, amount, description, date) => {
+  const query = `
+    INSERT INTO transactions (user_id, budget_id, amount, description, date, created_at, updated_at)
+    VALUES (?, ?, ?, ?, ?, NOW(), NOW())
+  `;
+  const [result] = await db.execute(query, [userId, budgetId, amount, description, date]);
+  return result;
+};
