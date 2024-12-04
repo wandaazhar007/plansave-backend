556c00d9dc3374df3ca100ae792e0c10bf82065c
wanda azhar <wandaazhar@gmail.com>
2024-12-03 23:00:38 -0700

    add TransactionRoutes.js

---
+++ routes/TransactionRoutes.js
@@ -0,0 +1,12 @@
+import express from "express";
+import { getTransactions, addTransaction } from "../controllers/TransactionController.js";
+
+const router = express.Router();
+
+// Get all transactions for the user
+router.get("/", getTransactions);
+
+// Add a new transaction
+router.post("/", addTransaction);
+
+export default router;
