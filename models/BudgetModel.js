c43e65487690d39826fbe041cbbeb570b3849e55
wanda azhar <wandaazhar@gmail.com>
2024-12-03 22:06:40 -0700

    add BudgetModel.js

---
+++ models/BudgetModel.js
@@ -0,0 +1,18 @@
+import { connectDB } from "../config/dbConfig.js";
+
+const db = await connectDB();
+
+export const getBudgetsByUserId = async (userId) => {
+  const query = `SELECT * FROM budgets WHERE user_id = ?`;
+  const [rows] = await db.execute(query, [userId]);
+  return rows;
+};
+
+export const createBudget = async (userId, category, subcategory, amount, timeframe) => {
+  const query = `
+    INSERT INTO budgets (user_id, category, subcategory, amount, timeframe, created_at, updated_at)
+    VALUES (?, ?, ?, ?, ?, NOW(), NOW())
+  `;
+  const [result] = await db.execute(query, [userId, category, subcategory, amount, timeframe]);
+  return result;
+};
